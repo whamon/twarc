217b81565fbebfb477e07c59773bd11d95f6afaf
Ed Summers <ehs@pobox.com>
2019-09-21 15:19:46 +0000

    encoding

--- utils/urls.py
+++ utils/urls.py
@@ -5,6 +5,7 @@ Print out the URLs in a tweet json stream.
 """
 from __future__ import print_function
 
+import sys
 import json
 import fileinput
 
@@ -12,8 +13,8 @@ for line in fileinput.input():
     tweet = json.loads(line)
     for url in tweet["entities"]["urls"]:
         if 'unshortened_url' in url:
-            print(url['unshortened_url'].encode('utf8'))
+            sys.stdout.write(url['unshortened_url'] + "\n")
         elif url.get('expanded_url'):
-            print(url['expanded_url'].encode('utf8'))
+            sys.stdout.write(url['expanded_url'] + "\n")
         elif url.get('url'):
-            print(url['url'].encode('utf8'))
+            sys.stdout.write(url['url'] + "\n")
